67a961981b92827ccf6c32a915d3a27862c215c8
shroukgamall17 <shrouqgamal17@gmail.com>
2024-09-24 14:46:14 +0300

    pull fix frontend commit

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ const todosRoutes = require('./routes/todos')
 const usersRoutes = require('./routes/users')
 
 //built in middleware
-app.use(cors({origin:'*',
-    //methods
-}))
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
 app.use(express.json())
 
 app.use("/todos",todosRoutes)
